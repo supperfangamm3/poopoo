aca04a3f7c3f821a0bc27f3d166e06f7dd1cadd5
supperfangamm3 <80982012+supperfangamm3@users.noreply.github.com>
2023-01-12 15:42:33 -0500

    Add files via upload

---
+++ Main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() 
+{
+  string supr = "supr";
+  string area = "bathroom";
+  
+  int x = 5;  
+  
+    cout << supr << endl;
+    cout << "go to the " << area << endl;
+    cout << "You have " << x << " seconds.";
+    return 0;
+}
+
